75273e72f9f65c767230577392b909fed1d4e7cb
shubhamphepale <shubhamphepale@gmail.com>
2021-07-22 12:28:54 +0530

    Add Shape

--- src/com/neosoft/basics/Shape.java
+++ src/com/neosoft/basics/Shape.java
@@ -1,6 +1,7 @@
 package com.neosoft.basics;
 
-public class Shape {
+
+class Shape1 {
 	
 	void Area() {
 		System.out.println("Calculate the area of shape");
@@ -10,41 +11,44 @@ public class Shape {
 		System.out.println("Draw the Shape with it coordinates");
 	}
 }
-class Circle extends Shape{
+class Circle extends Shape1{
 	int radius=5;
 	double area;
 	void Area() {
 		area=3.14*(radius*radius);
+		System.out.println("Calculated the area of Circle ="+area);
 	}
 	void Draw() {
 		System.out.println("Drawing a shape Circle with radius "+radius);
 	}
 }
-class Triangle extends Shape{
+class Triangle extends Shape1{
 	int height=20;
 	int base=10;
 	int area;
 	void Area() {
 		area=(height*base)/2;
+		System.out.println("Calculated the area of Triangle ="+area);
 	}
 	void Draw() {
 		System.out.println("Drawing a shape Circle with Height "+height+" and Base "+base);
 	}
 }
-class Square extends Shape{
+class Square extends Shape1{
 	int side=5;
 	int area;
 	void Area() {
 		area=(side*side);
+		System.out.println("Calculated the area of Square ="+area);
 	}
 	void Draw() {
 		System.out.println("Drawing a shape Circle with Side "+side);
 	}
 }
-class Test{  
+public class Shape{  
 	public static void main(String args[]){ 
 		
-		Shape s1;
+		Shape1 s1;
 		s1 = new Circle();
 		s1.Area();
 		s1.Draw();
@@ -54,6 +58,5 @@ class Test{
 		s1 = new Square();
 		s1.Area();
 		s1.Draw();
-	
 	}  
 }  
